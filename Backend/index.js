6f29a7d84648613d8443ba7db8511561fa577d7d
Mr. Alexander <100361766+Alexandergebremedhin123@users.noreply.github.com>
2025-08-04 09:54:46 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,17 +13,23 @@ require('./db')
 
 const allowedOrigins = [process.env.FRONTEND_URL];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
-
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+// }));
+const corsOptions = {
+  origin: [
+    'http://localhost:3000', 
+    'https://community-bulletin-board-3.onrender.com', 
+  ],
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET)); 
 
