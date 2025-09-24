8f0cdc9034d86a2c86735c158e3076a1c601c595
Mr. Alexander <100361766+Alexandergebremedhin123@users.noreply.github.com>
2025-09-24 01:26:21 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,25 +11,12 @@ const port=process.env.PORT
 
 require('./db')
 
-const allowedOrigins = [process.env.FRONTEND_URL];
+app.use(cors({
+  origin: "https://community-bulletin-board.vercel.app", 
+  methods: "GET,POST,PUT,DELETE",   
+  credentials: true              
+}));
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true,
-// }));
-const corsOptions = {
-  origin: [
-    'http://localhost:3000', 
-    'https://community-bulletin-board-3.onrender.com', 
-  ],
-};
-app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET)); 
 
